09a727c52420b22a71e02c1e6b14c8190cd18dcc
Marc Matteo <marcm@lectroid.net>
2015-09-11 09:47:56 -0700

    Convert to ESP8266 Thing + AM2302.

--- water_heater_monitor/water_heater_monitor.ino
+++ water_heater_monitor/water_heater_monitor.ino
@@ -2,76 +2,34 @@
  * Listens for serial data from an IMC Eagle solar water heater controller and
  * uploads it to a waiting server.
  *
- * Assumes an CC3000-based wireless network connectiom shield and serial
- * connection to athe Eagle controller via hardware serial.
+ * Assumes a Sparkfun EST8266 Thing and a serial connection to an Eagle controller via hardware serial.
  */
 
-#include <Adafruit_CC3000.h>
-#include "utility/debug.h"
-#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <Phant.h>
+#include <DHT.h>
 
 #include "keys.h"
 
-
-#define ADAFRUIT_CC3000_IRQ   3
-#define ADAFRUIT_CC3000_VBAT  5
-#define ADAFRUIT_CC3000_CS    10
-
-#define IDLE_TIMEOUT_MS       2000
-
-Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIVIDER);
-Adafruit_CC3000_Client client;
-
-unsigned long lastWebUpdateTime = 0;
+const int LED_PIN = 5;                 // Thing's onboard, green LED
+unsigned long lastUpdateTime = 0;
 char eagle_data[81];
 uint8_t data_index = 0;
 
+DHT dht = DHT(4, DHT22);               // Pin 4, DHT 22 (AM2302)
 
 void setup () {
-  Serial.begin(2400);
-  Serial.println(F("\nWater Heater Monitor"));
-
-  // CC3000 setup
-  Serial.print(F("Initializing network... "));
-  if (!cc3000.begin()) {
-    Serial.println(F("unable to initialise the CC3000!"));
-    while(1);
-  }
-
-  reconnectToAP();
-
-  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
-  if (!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv)) {
-    Serial.println(F("Unable to retrieve network info!"));
-  } else {
-    Serial.print(F("  IP Addr: ")); cc3000.printIPdotsRev(ipAddress); Serial.println();
-  }
-
-  initThermometer();
-
-  Serial.println(F("Waiting for data..."));
+  initHardware();
+  connectWiFi();
 }
-
 
 void loop () {
-  unsigned long now = millis();
-
-  // If we haven't updated in 5 minutes, send along a "PING" to keep showing something.
-  if (now > lastWebUpdateTime + 300000 || now < lastWebUpdateTime) {
-    //strcpy(eagle_data, "0.05  120.0  110.0  12.0  159.0  100.0  110.0  ON  OFF  ");
-    //updateWeb();
-
-    data_index = 0;
-    eagle_data[data_index] = '\0';
-    updateWeb();
-  }
+  delay(100);
 }
-
 
 void serialEvent() {
   while (Serial.available()) {
     char c = Serial.read();
-    if (c) UDR0 = c;
 
     // Assume a LF is the end of the line
     if (c == '\n' && strlen(eagle_data) > 0) {
@@ -83,8 +41,8 @@ void serialEvent() {
 
       unsigned long now = millis();
 
-      if ((now > lastWebUpdateTime + 60000 || now < lastWebUpdateTime) && strncmp(eagle_data, "RUNTIME", 7) != 0 && strchr(eagle_data, ':') != NULL) {
-        updateWeb();
+      if ((now > lastUpdateTime + 60000 || now < lastUpdateTime) && strncmp(eagle_data, "RUNTIME", 7) != 0 && strchr(eagle_data, ':') != NULL) {
+        postToPhant();
       }
 
       // Reset the data
@@ -103,85 +61,84 @@ void serialEvent() {
   }
 }
 
-
-void reconnectToAP () {
-  while (!cc3000.checkConnected()) {
-    if (cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-      Serial.println(F("AP connection successful."));
-      while (!cc3000.checkDHCP()) {
-        delay(100);
-      }
-    } else {
-      Serial.println(F("AP connection failure... attempting to reconnect..."));
-      delay(30000);
-    }
-  }
+void initHardware() {
+  Serial.begin(2400);
+  pinMode(LED_PIN, OUTPUT);
+  dht.begin();
 }
 
+void connectWiFi() {
+  byte ledStatus = LOW;
 
-void updateWeb() {
-  Serial.println(F("Sending data..."));
+  // Set WiFi mode to station (as opposed to AP or AP_STA)
+  WiFi.mode(WIFI_STA);
 
-  // The Adafruit CC3000 library only supports about 90 bytes in the fastrprint()
-  // method, therefore we need two char arrays to hold all the Eagle query args.
-  char eagle_query_args1[66] = "&runtime=0:00&coll_t=&stor_t=&diff_t=&hili_t=";
-  char eagle_query_args2[66] = "&aux_1=&aux_2=&pump=OFF&uplim=OFF&fault=";
+  // WiFI.begin([ssid], [passkey]) initiates a WiFI connection
+  // to the stated [ssid], using the [passkey] as a WPA, WPA2,
+  // or WEP passphrase.
+  WiFi.begin(WiFiSSID, WiFiPSK);
+
+  // Use the WiFi.status() function to check if the ESP8266
+  // is connected to a WiFi network.
+  while (WiFi.status() != WL_CONNECTED) {
+    // Blink the LED
+    digitalWrite(LED_PIN, ledStatus); // Write LED high/low
+    ledStatus = (ledStatus == HIGH) ? LOW : HIGH;
+
+    // Delays allow the ESP8266 to perform critical tasks
+    // defined outside of the sketch. These tasks include
+    // setting up, and maintaining, a WiFi connection.
+    delay(100);
+    // Potentially infinite loops are generally dangerous.
+    // Add delays -- allowing the processor to perform other
+    // tasks -- wherever possible.
+  }
+
+   digitalWrite(LED_PIN, LOW);
+}
+
+int postToPhant() {
+  char *tokenptr;
+  
+  // LED turns on when we enter, it'll go off when we
+  // successfully post.
+  digitalWrite(LED_PIN, HIGH);
+
+  // Declare an object from the Phant library - phant
+  Phant phant(PhantHost, PublicKey, PrivateKey);
 
   if (strlen(eagle_data) > 0) {
-    char *tokenptr;
-
-    sprintf(eagle_query_args1, "&runtime=%s&coll_t=%s&stor_t=%s&diff_t=%s&hili_t=%s",
-      strtok_r(eagle_data, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr)
-    );
-
-    sprintf(eagle_query_args2, "&aux_1=%s&aux_2=%s&pump=%s&uplim=%s&fault=%s",
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr),
-      strtok_r(NULL, " ", &tokenptr)
-    );
-  }
-
-  // Convert the temerature float to something fastrprint likes
-  char temp_str[7]; // "123.45"
-  dtostrf(getTemperature(), 1, 2, temp_str);
-
-  client.connect(PHANT_HOST, PHANT_HOST_PORT);
-  if (client.connected()) {
-    client.fastrprint(F("GET /input/")); client.fastrprint(PHANT_PUBLIC_KEY);
-    client.fastrprint(F("?private_key=")); client.fastrprint(PHANT_PRIVATE_KEY);
-    client.fastrprint(eagle_query_args1);
-    client.fastrprint(eagle_query_args2);
-    client.fastrprint(F("&ambient_t=")); client.fastrprint(temp_str);
-    client.fastrprint(F(" HTTP/1.1\r\n"));
-
-    client.fastrprint(F("Host: ")); client.fastrprintln(PHANT_HOST);
-
-    client.fastrprint("\r\n");
-
-    // Show the response
-    unsigned long lastRead = millis();
-    while (client.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
-      while (client.available()) {
-        char c = client.read();
-        Serial.print(c);
-        lastRead = millis();
-      }
-    }
-
-    client.close();
-    lastWebUpdateTime = millis();
-    Serial.println(F("Done."));
+    phant.add("runtime", strtok_r(eagle_data, " ", &tokenptr));
+    phant.add("coll_t",  strtok_r(NULL, " ", &tokenptr));
+    phant.add("stor_t",  strtok_r(NULL, " ", &tokenptr));
+    phant.add("diff_t",  strtok_r(NULL, " ", &tokenptr));
+    phant.add("hili_t",  strtok_r(NULL, " ", &tokenptr));
+    phant.add("aux_1",   strtok_r(NULL, " ", &tokenptr));
+    phant.add("aux_2",   strtok_r(NULL, " ", &tokenptr));
+    phant.add("pump",    strtok_r(NULL, " ", &tokenptr));
+    phant.add("uplim",   strtok_r(NULL, " ", &tokenptr));
+    phant.add("fault",   strtok_r(NULL, " ", &tokenptr));
   } else {
-    Serial.println(F("Web server connection failed."));
-    if (!cc3000.checkConnected()) {
-      Serial.println(F("Wifi appears down, attempting to reconnect..."));
-      reconnectToAP();
-    }
+    phant.add("runtime", "0:00");
+    phant.add("pump",    "OFF");
+    phant.add("uplim",   "OFF");
   }
+  phant.add("ambient_t", dht.readTemperature(true));
+
+  // Now connect to data.sparkfun.com, and post our data:
+  WiFiClient client;
+
+  if (!client.connect(PhantHost, PhantPort)) {
+    // If we fail to connect, return 0.
+    digitalWrite(LED_PIN, LOW);
+    return 0;
+  }
+
+  // If we successfully connected, print our Phant post:
+  client.print(phant.post());
+
+  // Before we exit, turn the LED off.
+  digitalWrite(LED_PIN, LOW);
+  return 1; // Return success
 }
+
